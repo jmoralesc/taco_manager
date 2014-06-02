2bcba0165b11054cd86ca1ff32d206d5cc92c79f
Julian Morales <julian.morales@crowdint.com>
2014-06-02 12:53:48 -0500

    Add more s3 configs

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,9 +7,10 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],                        
     :region                 => ENV['AWS_REGION'],
   }
+
+  config.cache_dir            = "#{Rails.root}/tmp/uploads"
+
   config.fog_directory        = ENV['AWS_BUCKET']                
   config.fog_attributes       = {'Cache-Control'=>'max-age=315576000'} 
-  config.fog_s3_access_policy = :public_read
-  config.fog_host             = "#{ENV['AWS_ASSET_URL']}/#{ENV['AWS_BUCKET']}"
-  config.storage        = Rails.env.production? ? :fog : :file 
+  config.storage              = Rails.env.production? ? :fog : :file 
 end
