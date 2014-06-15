2b5f35edaa6dc62dffebd0ca1ea6ed2ea4d7a62f
Julian Morales <julia.morales@crowdint.com>
2014-06-15 16:02:48 -0500

    show order added

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ActiveRecord::Base
   belongs_to :user
+  belongs_to :food_place
   has_many :menu_line_items
   accepts_nested_attributes_for :menu_line_items, :reject_if => :all_blank, :allow_destroy => true
 end
