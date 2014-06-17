336aa728f96acaff4ebcd24e23a1fc30cb91c4b2
Julian Morales <julia.morales@crowdint.com>
2014-06-17 17:04:53 -0500

    url added to mail

--- app/mailers/user_mail.rb
+++ app/mailers/user_mail.rb
@@ -1,9 +1,9 @@
 class UserMail < ActionMailer::Base
   default from: 'julian.morales@crowdint.com'
 
-  def invitation(user)
-    @user = user
-    @url  = 'http://example.com/login'
+  def invitation(user, order)
+    @user = user 
+    @url  = link_to "order now", edit_order_path(order)
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
