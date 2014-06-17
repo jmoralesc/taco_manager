81a296edef90a355e1414ae8260674d428057ccd
Julian Morales <julia.morales@crowdint.com>
2014-06-17 16:18:40 -0500

    Payed menu line item

--- db/migrate/20140615231326_add_payed_to_menu_line_item.rb
+++ db/migrate/20140615231326_add_payed_to_menu_line_item.rb
@@ -1,5 +1,5 @@
 class AddPayedToMenuLineItem < ActiveRecord::Migration
   def change
-  	change_column :menu_line_items, :payed, :integer, :default => 0
+  	create_column :menu_line_items, :payed, :integer, :default => 0
   end
 end
