35f7c33a91ca7e164bef095b1268fd59714868c6
Julian Morales <julia.morales@crowdint.com>
2014-06-17 17:29:15 -0500

    url to mail issue

--- app/mailers/user_mail.rb
+++ app/mailers/user_mail.rb
@@ -1,9 +1,11 @@
 class UserMail < ActionMailer::Base
   default from: 'julian.morales@crowdint.com'
+  default_url_options[:host] = 'heroku.com'
 
   def invitation(user, order)
     @user = user 
-    @url  = edit_order_path(order)
+    @order = order
+    @url  = edit_order_url(@order)
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
