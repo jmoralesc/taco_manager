f2db479bac553fea2886441115f683a792dda688
Julian Morales <julia.morales@crowdint.com>
2014-06-22 03:21:05 -0500

    ratings finished, conflic level solved

--- db/migrate/20140622044642_food_place_stars.rb
+++ db/migrate/20140622044642_food_place_stars.rb
@@ -1,6 +1,6 @@
 class FoodPlaceStars < ActiveRecord::Migration
   def change
-  	remove_column :food_places, :level
+
   	add_column :food_places, :stars, :decimal
   	add_column :food_places, :times_rated, :integer
   end
